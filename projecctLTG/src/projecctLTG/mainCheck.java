4636fdb7d80246980c53b4b88896fb3561e179b0
NKloos <2020633@stud.hs-mannheim.de>
2021-03-26 14:54:56 +0100

    at 14:54 after pull

--- projecctLTG/src/projecctLTG/mainCheck.java
+++ projecctLTG/src/projecctLTG/mainCheck.java
@@ -4,7 +4,8 @@ public class mainCheck {
 
 	public static void main(String[] args) {
 		// this is test 1
-		//this is tes 2 (from github)
+		//this is test 2 (from github)
+		// this is test 3
 		
 
 	}
