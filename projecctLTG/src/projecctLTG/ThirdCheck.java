f5f1a13e79f27c004b828ccafe58893304554661
NKloos <2020633@stud.hs-mannheim.de>
2021-03-26 15:00:20 +0100

    15:00

--- projecctLTG/src/projecctLTG/ThirdCheck.java
+++ projecctLTG/src/projecctLTG/ThirdCheck.java
@@ -5,6 +5,7 @@ public class ThirdCheck {
 	public static void main(String[] args) {
 		// distracted-_-
 		//upd: geht so 
+		// wird es jetzt gespeichert?
 		System.out.println("hat geklappt");
 	}
 
